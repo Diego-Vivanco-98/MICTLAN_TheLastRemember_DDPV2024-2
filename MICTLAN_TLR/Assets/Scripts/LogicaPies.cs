b18b271f7cb2f01a807a8f947d63031b92abd6f9
Diego-Vivanco-98 <diegovqd@gmail.com>
2024-12-21 17:15:24 -0600

    Se implementa logica de Salto del Personaje
    Se implementan dos animaciones para el salto.
    Se codifica la logica del salto.
    Se modifica el Animator.

---
+++ MICTLAN_TLR/Assets/Scripts/LogicaPies.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogicaPies : MonoBehaviour
+{
+    public PlayerProta prota;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        prota.puedoSaltar  = true;
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        prota.puedoSaltar =false;
+    }
+}
--- MICTLAN_TLR/Assets/Scripts/PlayerProta.cs
+++ MICTLAN_TLR/Assets/Scripts/PlayerProta.cs
@@ -4,17 +4,30 @@ using UnityEngine;
 
 public class PlayerProta : MonoBehaviour
 {
+    public Rigidbody rb;
     public float velocidadMovimiento = 10.0f;
     public float velocidadRotacion = 200.0f;
 
     private Animator anim;
     public float x, y;
 
+    public float fuerzaDeSalto = 8.0f;
+    public bool puedoSaltar;
+
+
+    void FixedUpdate()
+    {
+        transform.Rotate(0, x * Time.deltaTime * velocidadRotacion, 0);
+        transform.Translate(0, 0, y * Time.deltaTime * velocidadMovimiento);
+    }
+
 
 
     // Start is called before the first frame update
     void Start()
     {
+        puedoSaltar = false;
+        rb = GetComponent<Rigidbody>();
         anim = GetComponent<Animator>();
         
     }
@@ -25,10 +38,32 @@ public class PlayerProta : MonoBehaviour
         x = Input.GetAxis("Horizontal");
         y = Input.GetAxis("Vertical");
 
-        transform.Rotate(0,x*Time.deltaTime*velocidadRotacion, 0);
-        transform.Translate(0, 0, y*Time.deltaTime*velocidadMovimiento);
 
         anim.SetFloat("velX", x);
         anim.SetFloat("velY", y);
+
+        if (puedoSaltar)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                
+                rb.AddForce(new Vector3(0, fuerzaDeSalto, 0), ForceMode.Impulse);
+            }
+            anim.SetBool("tocoSuelo", true);
+
+        }
+        else
+        {
+            EstoyCayendo();
+            //anim.SetBool("salte", true);
+
+        }
+
+    }
+
+    public void EstoyCayendo()
+    {
+        anim.SetBool("tocoSuelo", false);
+        anim.SetBool("tocoSuelo", false);
     }
 }
