91a10e9099f9ff2fdd45fe0028d2df320010231d
Diego-Vivanco-98 <diegovqd@gmail.com>
2024-12-22 12:19:44 -0600

    Scripts para efectos de Sonido y Musica Ambiental
    Se crean dos scripts para la lógica de los efectos de sonido y música ambiental del videojuego.

---
+++ MICTLAN_TLR/Assets/Scripts/SoundSFxProta.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundSFxProta : MonoBehaviour
+{
+    public AudioSource SFxProtaSource;
+
+    public AudioClip[] SFxClip;
+
+    public static SoundSFxProta InstanceSFxProta;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void Awake()
+    {
+        InstanceSFxProta = this;
+    }
+
+    public void caminaProta()
+    {
+        SFxProtaSource.PlayOneShot(SFxClip[0]);
+    }
+
+    public void saltoProta()
+    {
+        SFxProtaSource.PlayOneShot(SFxClip[1]);
+    }
+
+    public void impactaSueloProta()
+    {
+        SFxProtaSource.PlayOneShot(SFxClip[2]);
+    }
+
+    public void golpeaProta()
+    {
+        SFxProtaSource.PlayOneShot(SFxClip[3]);
+    }
+    public void pateaProta()
+    {
+        SFxProtaSource.PlayOneShot(SFxClip[4]);
+    }
+
+}
