08934d6ccbbe242fed92f7d582d6ae317dd8fb15
Diego-Vivanco-98 <diegovqd@gmail.com>
2024-12-27 15:16:22 -0600

    Scritpts Inventario P2
    Se crean los scripts para el manejo de objetos de tipo Item y de tipo Slot.

--- MICTLAN_TLR/Assets/Scripts/interfaces/InventarioArmas/Inventario.cs
+++ MICTLAN_TLR/Assets/Scripts/interfaces/InventarioArmas/Inventario.cs
@@ -20,7 +20,13 @@ public class Inventario : MonoBehaviour
         for(int i=0; i < numSlots; i++)
         {
             slot[i] = slotHolder.transform.GetChild(i).gameObject;
+
+            if (slot[i].GetComponent<Slot>().item = null)
+            {
+                slot[i].GetComponent<Slot>().vacio = true;
+            }
         }
+
 
     }
 
@@ -46,6 +52,39 @@ public class Inventario : MonoBehaviour
             Time.timeScale = 1;
             Cursor.visible = false;
             Cursor.lockState = CursorLockMode.Locked;
+        }
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Item")
+        {
+            GameObject itemPickedUp = other.gameObject;
+            Item item = itemPickedUp.GetComponent<Item>();
+            AddItem(itemPickedUp, item.ID, item.type, item.descripcion, item.icono);
+        }
+        
+    }
+
+    public void AddItem(GameObject itemObject, int itemID, string itemType, string itemDescripcion, Sprite itemIcono)
+    {
+        for(int i = 0; i < numSlots; i++)
+        {
+            if (slot[i].GetComponent<Slot>().vacio)
+            {
+                itemObject.GetComponent<Item>().pickedUp = true;
+                slot[i].GetComponent<Slot>().item = itemObject;
+                slot[i].GetComponent<Slot>().ID = itemID;
+                slot[i].GetComponent<Slot>().type = itemType;
+                slot[i].GetComponent<Slot>().descripcion = itemDescripcion;
+                slot[i].GetComponent<Slot>().icono = itemIcono;
+
+                itemObject.transform.parent = slot[i].transform;
+                itemObject.SetActive(false);
+
+                slot[i].GetComponent<Slot>().vacio = false; 
+            }
         }
     }
 }
---
+++ MICTLAN_TLR/Assets/Scripts/interfaces/InventarioArmas/Slot.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slot : MonoBehaviour
+{
+    public GameObject item;
+
+    public int ID;
+    public string type;
+    public string descripcion;
+    public bool vacio;
+    public Sprite icono;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
